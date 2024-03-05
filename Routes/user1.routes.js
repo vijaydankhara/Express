cf2d4dfd580eaeac5177322cb8f606d386e86c17
vijay dankhara <vijaydankhara111@gmail.com>
2024-03-05 22:48:33 +0530

    05-03-2024

--- Routes/user1.routes.js
+++ Routes/user1.routes.js
@@ -1,7 +1,17 @@
-const express = require('express');
+const express = require("express");
 const userRoutes = express.Router();
-const {addUser} = require('../controller/user1.controller');
+const {
+  addUser,
+  getAllUsers,
+  getUser,
+  updateUser,
+  deleteUser,
+} = require("../controller/user1.controller");
 
-userRoutes.post( '/add-user', addUser);
+userRoutes.post("/add-user", addUser);
+userRoutes.get("/get-all-users",getAllUsers);
+userRoutes.get("/get-user",getUser);
+userRoutes.put("/update-user",updateUser);
+userRoutes.delete('/delete-user', deleteUser);
 
-module.exports = userRoutes; 
+module.exports = userRoutes;
--- controller/user1.controller.js
+++ controller/user1.controller.js
@@ -18,3 +18,63 @@ try{
         res.status(500).json({message: 'Internal Server Error !!!'});
 }
 }
+
+exports.getAllUsers = async(req,res)=>{
+    try{
+        let users = await User.find();
+        res.status(200).json(users);
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({ message: "Internal Server Error !!! "});
+    }
+};
+
+exports.getUser = async(req,res) =>{
+    try{
+        let userId = req.query.userid;
+        let user = await User.findById(userId);
+        // let user = await User.findone({ firstName: userId});
+        if (!user) {
+            return res.status(404).json({ message: 'User Is Not Found'});
+        }
+        res.status(200).json(user);
+    }catch (error){
+        console.log(error);
+        res.status(500).json({ message: 'Internal Server Error !!! '})
+    }
+};
+
+
+
+exports.updateUser = async (req,res) => {
+try{
+    let userId = req.query.userId;
+    let user = await User.findById(userId);
+    if (!user) {
+        return res.status(404).json({ message: 'User Is Not Found'});
+    }
+    // user = await User.findByIdAndUpdate(user._id,{$set: { req.body }}, {new:true}); 
+user = await User.findOneAndUpdate({_id:user._id},{$set: {...req.body }}, {new:true}); 
+    res.status(200).json({user, message: 'User Is Updated....'});
+}catch (error){
+    console.log(error);
+    res.status(500).json({ message: 'Internal Server Error !!! '})
+}    
+};
+exports.deleteUser = async(req,res)=>{
+    try{
+        let userId = req.query.userId;
+        let user = await User.findById(userId);
+            if (!user) {
+                return res.status(404).json({ message: 'User Is Not Found'});
+            }
+            res.status(200).json(user);
+        user = await User.findOneAndDelete({_id:user._id});
+        res.status(200).json({user, message: 'User Is Deleted... '});        
+        }catch (error){
+            console.log(error);
+            res.status(500).json({ message: 'Internal Server Error !!! '})
+        }
+
+};
+
